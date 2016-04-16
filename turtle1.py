714cba9a8f67ae2003e5c6d2a040acbaa317636d
QaziPython <rafehqazi1@gmail.com>
2016-04-16 08:05:07 -0500

    renamed turtle2.py to turtle1.py. Removed turtle1.py

--- turtle1.py
+++ turtle1.py
@@ -1,14 +1,20 @@
 import turtle
 
 
-brad = turtle.Turtle()
-brad.begin_fill()
-brad.screen.bgcolor('red')
-brad.color("white", "green")
-brad.fillcolor('white')
-brad.speed(100)
-for i in range(5):
-    brad.forward(30)
-    brad.right(144)
-brad.end_fill()
-turtle.done()
+def draw_star(size, color):
+    angle = 120
+    turt = turtle.Turtle()
+    turt.speed(100)
+    turt.fillcolor(color)
+    turt.screen.bgcolor('red')
+    turt.begin_fill()
+
+    for side in range(5):
+        turt.forward(size)
+        turt.right(angle)
+        turt.forward(size)
+        turt.right(72 - angle)
+    turt.end_fill()
+    turtle.done()
+
+draw_star(6, "white")
