1b69d27c57c9e3205d02b44114a9e44f4817416f
QaziPython <rafehqazi1@gmail.com>
2016-04-16 09:11:24 -0500

    50 starts being drawn. Need to space them out.

--- turtle1.py
+++ turtle1.py
@@ -1,20 +1,66 @@
 import turtle
 
 
-def draw_star(size, color):
+def draw_star(size, color, turtle_, speed_=100):
     angle = 120
-    turt = turtle.Turtle()
-    turt.speed(100)
-    turt.fillcolor(color)
-    turt.screen.bgcolor('red')
-    turt.begin_fill()
+    turtle_.speed(speed_)
+    turtle_.fillcolor(color)
+    turtle_.begin_fill()
 
     for side in range(5):
-        turt.forward(size)
-        turt.right(angle)
-        turt.forward(size)
-        turt.right(72 - angle)
-    turt.end_fill()
+        turtle_.forward(size)
+        turtle_.right(angle)
+        turtle_.forward(size)
+        turtle_.right(72 - angle)
+
+    turtle_.end_fill()
+
+
+def row_5(turtle_, size):
+    for i in range(5):
+        draw_star(size, "white", turtle_)
+        turtle_.forward(20)
+
+    turtle_.forward(8)
+    turtle_.left(120)
+    turtle_.forward(7)
+    turtle_.left(60)
+    turtle_.forward(15)
+
+
+def row_6(turtle_, size):
+    for i in range(6):
+        draw_star(size, "white", turtle_)
+        turtle_.forward(20)
+
+    turtle_.right(120)
+    turtle_.forward(20)
+    turtle_.right(60)
+    turtle_.forward(20)
+
+
+def star_row(turtle_, size=6):
+    row_6(turtle_, size)
+    row_5(turtle_, size)
+    row_6(turtle_, size)
+    row_5(turtle_, size)
+    row_6(turtle_, size)
+    row_5(turtle_, size)
+    row_6(turtle_, size)
+    row_5(turtle_, size)
+
+    for i in range(6):
+        draw_star(size, "white", turtle_)
+        turtle_.forward(20)
+
+
+def main():
+    turt = turtle.Turtle()
+    turt.screen.bgcolor('blue')
+    turt.pencolor('blue')
+    star_row(turt, size=4)
     turtle.done()
 
-draw_star(6, "white")
+
+if __name__ == '__main__':
+    main()
